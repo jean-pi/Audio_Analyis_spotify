71621cbff20ccf30342a71161cebe42dc32d03ab
Jean Pierre Veliz Lavayen <jeanpierreveliz26@gmail.com>
2026-05-02 20:19:38 -0500

    maybe last commit

--- src/constants/lang_es.ts
+++ src/constants/lang_es.ts
@@ -11,7 +11,7 @@ export const IMG_SPOTIFY = {
 }
 
 // warning privacy
-export const TEXT_WARNING_PRIVACY = "By logging in you agree to this website having access to data and activity from your Spotify account"
+export const TEXT_WARNING_PRIVACY = "By logging in you agree to this website having access to data and activity from your Spotify account. "
 export const LINK_WARNING_PRIVACY = {
     text: "Spotify privacy policy",
     url: "https://www.spotify.com/us/legal/privacy-policy/"
